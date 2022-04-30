024371f087b185f6a993c6639abd9576dd343532
fidellim <lim.fidel@gmail.com>
2022-04-30 22:07:18 +0400

    set navbar as fixed position

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Cart from "./Cart";
 import { AiOutlineShopping } from "react-icons/ai";
 import { useStateContext } from "../context/StateContext";
 
@@ -7,15 +8,23 @@ const NavBar = () => {
 	const { showCart, setShowCart, totalQuantities } = useStateContext();
 
 	return (
-		<div className="navbar-container">
-			<p className="logo">
-				<Link href="/">Ecommerce</Link>
-			</p>
+		<div className="navbar-wrapper">
+			<div className="navbar-container">
+				<p className="logo">
+					<Link href="/">Ecommerce</Link>
+				</p>
 
-			<button type="button" className="cart-icon" onClick="">
-				<AiOutlineShopping />
-				<span className="cart-item-qty">{totalQuantities}</span>
-			</button>
+				<button
+					type="button"
+					className="cart-icon"
+					onClick={() => setShowCart((prev) => !prev)}
+				>
+					<AiOutlineShopping />
+					<span className="cart-item-qty">{totalQuantities}</span>
+				</button>
+
+				{showCart && <Cart />}
+			</div>
 		</div>
 	);
 };
