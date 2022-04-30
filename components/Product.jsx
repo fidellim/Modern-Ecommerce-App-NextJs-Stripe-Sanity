cececeaaee0d0117d916389282e3d9f198367c34
fidellim <lim.fidel@gmail.com>
2022-04-30 19:38:54 +0400

    add product component

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
 
-const Product = () => {
-	return <div>Product</div>;
+const Product = ({ product: { image, name, slug, price } }) => {
+	return (
+		<div>
+			<Link href={`/product/${slug.current}`}>
+				<div className="product-card">
+					<div className="product-image-container">
+						<img
+							src={urlFor(image && image[0])}
+							width={250}
+							height={250}
+							classname="product-image"
+							alt=""
+						/>
+					</div>
+					<p className="product-name">{name}</p>
+					<p className="product-price">${price}</p>
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default Product;
