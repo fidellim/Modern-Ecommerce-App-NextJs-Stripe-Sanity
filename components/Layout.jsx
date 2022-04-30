70ac9dcb45f510210546f271b8f063d63763f6af
fidellim <lim.fidel@gmail.com>
2022-04-30 19:38:20 +0400

    add layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Head from "next/head";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
-const Layout = () => {
-	return <div>Layout</div>;
+const Layout = ({ children }) => {
+	return (
+		<div className="layout">
+			<Head>
+				<title>Ecommerce Store</title>
+			</Head>
+			<header>
+				<NavBar />
+			</header>
+			<main className="main-container">{children}</main>
+			<footer>
+				<Footer />
+			</footer>
+		</div>
+	);
 };
 
 export default Layout;
