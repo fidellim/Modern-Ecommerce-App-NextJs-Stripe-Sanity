ac19b255a89571a6070efa6831ec87ee50900566
fidellim <lim.fidel@gmail.com>
2022-04-30 19:37:11 +0400

    add layout, state context and toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Layout } from "../components";
 import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
+import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<StateContext>
+			<Layout>
+				<Toaster />
+				<Component {...pageProps} />
+			</Layout>
+		</StateContext>
+	);
 }
 
 export default MyApp;
