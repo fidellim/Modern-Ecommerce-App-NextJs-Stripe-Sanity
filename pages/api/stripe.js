8d7ef502eae9ab68144f6b4cc6cf9a371ec5a355
fidellim <lim.fidel@gmail.com>
2022-05-01 15:13:36 +0400

    change route when cancelled

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
 					};
 				}),
 				success_url: `${req.headers.origin}/success`,
-				cancel_url: `${req.headers.origin}/canceled`,
+				cancel_url: `${req.headers.origin}/`,
 			};
 
 			// Create Checkout Sessions from body params.
